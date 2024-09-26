2546600c86d30854c3b4fc5e0f8f1beee8bfc5be
Itachiuchiha786786 <166844972+Itachiuchiha786786@users.noreply.github.com>
2024-09-26 23:29:51 +0530

    Update mustjoin.py

--- SACHINxSANATANIxMUSIC/plugins/bot/mustjoin.py
+++ SACHINxSANATANIxMUSIC/plugins/bot/mustjoin.py
@@ -6,7 +6,7 @@ from SACHINxSANATANIxMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "WE_NXT"
+MUST_JOIN = "dynamic_gangs"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
